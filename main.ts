d62889f81a1db75439a49d0c2415ba4d9b648081
code243031 <koy321@naver.com>
2023-04-20 22:24:22 +0900

    프로젝트 초기화

---
+++ main.ts
@@ -0,0 +1,24 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+ 
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 960,
+        webPreferences: { preload: path.join(__dirname, 'preload.ts') }
+    });
+
+    win.loadFile('index.html');
+}
+ 
+app.whenReady().then(() => {
+    createWindow();
+ 
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+ 
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
